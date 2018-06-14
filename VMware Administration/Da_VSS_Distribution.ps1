47e455eee9db83e9d3b35c5da58dbf6c64484d81
Cameron Joyce <camerontjoyce@gmail.com>
2018-06-14 10:41:42 -0500

    DA_VSS 1.1
    Updated tab formatting.

--- VMware Administration/Da_VSS_Distribution.ps1
+++ VMware Administration/Da_VSS_Distribution.ps1
@@ -24,41 +24,41 @@ Catch{
 }
 
 # Attempt to connect to each VM and install the MSI package.
-    If(Test-Connection -ComputerName $vm -count 1 -Quiet){
-        Write-Host "Checking WMI on $vm"}
-        # Try / Catch block for WMI errors. A client that passes Test-Connection may not have PSRemoting enabled and will error. This will handle that.
-        Try{
-            $ErrorActionPreference = "Stop"
-            Invoke-Command -ComputerName $vm -ScriptBlock {If(!(Test-Path "C:\Temp")){New-Item "C:\Temp" -Type Directory}}
-        }
-        Catch [System.Management.Automation.Remoting.PSRemotingTransportException]{
-            Write-Warning "$vm failed connecting to WMI."
-            Write-Output "$vm Failed on WMI" | Out-File "C:\Temp\failed_$date.txt" -Append
-            Continue
-        }
-        Finally{
-            $ErrorActionPreference = "Continue"
-        }
+If(Test-Connection -ComputerName $vm -count 1 -Quiet){
+    Write-Host "Checking WMI on $vm"
+}
+    
+# Try / Catch block for WMI errors. A client that passes Test-Connection may not have PSRemoting enabled and will error. This will handle that.
+Try{
+    $ErrorActionPreference = "Stop"
+    Invoke-Command -ComputerName $vm -ScriptBlock {If(!(Test-Path "C:\Temp")){New-Item "C:\Temp" -Type Directory}}
+}
+Catch [System.Management.Automation.Remoting.PSRemotingTransportException]{
+    Write-Warning "$vm failed connecting to WMI."
+    Write-Output "$vm Failed on WMI" | Out-File "C:\Temp\failed_$date.txt" -Append
+    Continue
+}
+Finally{
+    $ErrorActionPreference = "Continue"
+}
 
 # Install DA VSS                
-                Write-Host "Checking: \\$vm\c$\Temp exists" -ForegroundColor Yellow
-                If(!(Test-Path "\\$vm\C$\Temp\"))
-                {
-                    Write-Host "\\$vm\c$\Temp DOES NOT EXIST ->> CREATING DIRECTORY \\$vm\c$\Temp" -ForegroundColor Yellow
-                    New-Item "\\$vm\c$\Temp" -ItemType Directory
-                }
-                Else
-                    {
-                        Write-Host "C:\Temp DOES EXIST" -ForegroundColor Yellow
-                    }
+Write-Host "Checking: \\$vm\c$\Temp exists" -ForegroundColor Yellow
+If(!(Test-Path "\\$vm\C$\Temp\")){
+    Write-Host "\\$vm\c$\Temp DOES NOT EXIST ->> CREATING DIRECTORY \\$vm\c$\Temp" -ForegroundColor Yellow
+    New-Item "\\$vm\c$\Temp" -ItemType Directory
+}
+Else{
+    Write-Host "C:\Temp DOES EXIST" -ForegroundColor Yellow
+}
 
-                Write-Host "Copying DAVSS.msi to \\$vm\c$\Temp" -ForegroundColor Yellow
-                Copy-Item "C:\temp\davss.msi" -Destination "\\$vm\C$\Temp\davss.msi " -force
+Write-Host "Copying DAVSS.msi to \\$vm\c$\Temp" -ForegroundColor Yellow
+Copy-Item "C:\temp\davss.msi" -Destination "\\$vm\C$\Temp\davss.msi " -force
 
-                Write-Host "Modifying UAC for installation" -ForegroundColor Yellow
-                Invoke-Command -ComputerName $vm -ScriptBlock {& cmd.exe /c "C:\Windows\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f"}
+Write-Host "Modifying UAC for installation" -ForegroundColor Yellow
+Invoke-Command -ComputerName $vm -ScriptBlock {& cmd.exe /c "C:\Windows\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f"}
 
-                Write-Host "Installing MSI package" -ForegroundColor Yellow
-                Invoke-Command -ComputerName $vm -ArgumentList $damgmtfloat, $dapw -ScriptBlock {& cmd.exe /c "C:\Temp\davss.msi /quiet NETSHELF_IP=$($args[0]) ADMIN_PASSWD=$($args[1])"}
+Write-Host "Installing MSI package" -ForegroundColor Yellow
+Invoke-Command -ComputerName $vm -ArgumentList $damgmtfloat, $dapw -ScriptBlock {& cmd.exe /c "C:\Temp\davss.msi /quiet NETSHELF_IP=$($args[0]) ADMIN_PASSWD=$($args[1])"}
 
-                Write-Host "Installation of Datrium VSS Agent SUCCESSFUL on $VM" -ForegroundColor Green
+Write-Host "Installation of Datrium VSS Agent SUCCESSFUL on $VM" -ForegroundColor Green
