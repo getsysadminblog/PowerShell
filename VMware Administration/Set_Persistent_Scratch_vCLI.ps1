e9405737976b18373935787bf28cd24f05ac20fc
Cameron Joyce <camerontjoyce@gmail.com>
2019-09-10 22:44:07 -0500

    First update in a long time

---
+++ VMware Administration/Set_Persistent_Scratch_vCLI.ps1
@@ -0,0 +1,8 @@
+#################################################################
+#   Setup MS Storage Replica Server 2016                        #
+#   Created by - Cameron Joyce                                  #
+#   Last Modified - Jul 22 2018                                 #
+#################################################################
+# This script is used to setup Volume Replication in Server 2016. Volume replication can be used in place of
+# DFS-R or a robocopy sync script, however it is recommended to know the cavieats before replacing traditional
+# tree mirroring with block mirroring.
---
+++ Windows Administration/Windows_Update.ps1
@@ -0,0 +1,26 @@
+#################################################################
+#   Automatic Windows Update Install                            #
+#   Created by - Cameron Joyce                                  #
+#   Last Modified - Sept 10 2019                                #
+#################################################################
+# This script is used to automatically install windows updates.
+# All dependencies will be automatically installed if they don't exist. 
+
+# Check that the Module exists.
+If (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Write-Host "Module installed"
+} 
+Else {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.0.5.201 -Force
+    Install-Module PSWindowsUpdate
+}
+
+# Check if Microsoft Update Service is added to WUServiceManager
+Try{
+    Get-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
+}
+Catch{
+    Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
+}
+
+Install-Windowsupdate –MicrosoftUpdate -Install –AcceptAll –AutoReboot
